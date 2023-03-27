d091842342c188318cd86a1f28fa209d93ffd6a1
Aljo-5 <99000107+Aljo-5@users.noreply.github.com>
2023-03-27 15:33:51 +0530

    Update sortStrings.java

--- sortStrings.java
+++ sortStrings.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
 import java.util.Arrays;
-
-public class sortStrings {
+public class sort {
 public static void main(String[] args) {
-String[] words = {"orange", "banana", "apple", "peach", "grape"};
-
-// Sort the array in alphabetical order
-Arrays.sort(words);
-
-// Print the sorted array
-for (String word : words) {
-System.out.println(word);
+int i,j;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of words");
+int num=sc.nextInt();
+String word[]=new String[num];
+sc.nextLine();
+for( i=0;i<num;i++){
+System.out.println("\nEnter a Word\n");
+word[i]=sc.nextLine();
 }
+for( i=0;i<num-1;i++){
+for( j=i+1;j<num;j++){
+if(word[i].compareTo(word[j])>0){
+String temp = word[i];
+word[i]=word[j];
+word[j]=temp;
+}
+}
+}
+System.out.println("Sorted Strings="+Arrays.toString(word));
+
+
 }
 }
