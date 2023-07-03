d3ced2fda150d979d0bec519f5b68e1906ba7419
Aljo Johny <99000107+Aljo-5@users.noreply.github.com>
2023-07-03 15:19:46 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,39 @@
-class CPU {
-    double price;
-    // nested class
-    class Processor{
-        // members of nested class
-        double cores;
-        String manufacturer;
-        double getCache(){
-            return 4.3;
-        }
-    }
-    // nested protected class
-    protected class RAM{
-        // members of protected nested class
-        double memory;
-        String manufacturer;
-        double getClockSpeed(){
-            return 5.5;
-        }
+
+import java.util.Scanner;
+
+class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        // create object of Outer class CPU
-        CPU cpu = new CPU();
-       // create an object of inner class Processor using outer class
-        CPU.Processor processor = cpu.new Processor();
-        // create an object of inner class RAM using outer class CPU
-        CPU.RAM ram = cpu.new RAM();
-        System.out.println("Processor Cache = " + processor.getCache());
-        System.out.println("Ram Clock speed = " + ram.getClockSpeed());
-        
-   }
-}        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            try {
+                System.out.print("Enter a positive integer: ");
+                int num = scanner.nextInt();
+
+                if (num < 0) {
+                    throw new NegativeNumberException("Negative numbers are not allowed.");
+                }
+
+                sum += num;
+            } catch (NegativeNumberException e) {
+                System.out.println(e.getMessage());
+                i--; // Decrement the loop counter to re-enter the current iteration.
+            }
+        }
+
+        double average = (double) sum / n;
+        System.out.println("The average is: " + average);
+
+        scanner.close();
+    }
+}
+
